12c7384e43767c352ba6b74b7cc94ab761e23256
poberherr <patrick.oberherr@gmail.com>
2015-01-17 11:09:01 +0100

    Add location search spinner

--- app/src/main/java/com/curricle/waypoints/NewWaypoint.java
+++ app/src/main/java/com/curricle/waypoints/NewWaypoint.java
@@ -1,8 +1,11 @@
 package com.curricle.waypoints;
 
+import android.app.ProgressDialog;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.android.gms.location.LocationListener;
@@ -13,6 +16,7 @@ public class NewWaypoint extends ActionBarActivity implements LocationListener {
     private TextView mLocText;
     private Location mMostAccurateLocation;
     private LocationRequestWrapper mLocationWrapper;
+    private ProgressBar locationSearchProgressBar;
 
     @Override
     protected void onResume() {
@@ -27,12 +31,14 @@ public class NewWaypoint extends ActionBarActivity implements LocationListener {
         setContentView(R.layout.activity_new_waypoint);
         mLocationWrapper = new LocationRequestWrapper(this, this);
         mLocText = (TextView) findViewById(R.id.position_label);
+        locationSearchProgressBar = (ProgressBar) findViewById(R.id.waitingForLocation);
     }
 
     @Override
     public void onLocationChanged(Location location) {
         if (mMostAccurateLocation == null) {
             mMostAccurateLocation = location;
+            locationSearchProgressBar.setVisibility(View.GONE);
         }
 
         if (location.getAccuracy() <= mMostAccurateLocation.getAccuracy()) {
